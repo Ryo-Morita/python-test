36700a6d87e4c4af17db1a7f844a9879f4d612b0
Ryo-Morita <morita.ryo2723@gmail.com>
2023-09-12 23:44:48 +0900

    add clasmethod in test_scaler.py

--- tests/test_scaler.py
+++ tests/test_scaler.py
@@ -2,17 +2,21 @@ from src.scaler import MyStandardScaler
 import numpy as np
 
 # test前にする処理をsetupというらしい。
-# 以下のsetupはpytestのお作法になっているわけでなないので、注意(Xを毎回定義しているので面倒な形になってる)
+# 以下のsetupはpytestのお作法になっているわけでなないかも。
+# .setup_class()で事前の処理を書けるっぽい
 
 class TestMyStandardScaler(object):
+    @classmethod
+    def setup_class(cls):
+        print("Start")
+        cls.scaler = MyStandardScaler()
+        cls.X1 = np.array([[-1,-2,-3],[0,0,0],[1,2,3]])
+        cls.X2 = np.array([[0,0,0],[2,2,2]])
+
     def test_mu(self):
-        X = np.array([[-1,-2,-3],[0,0,0],[1,2,3]])
-        scaler = MyStandardScaler()
-        scaler.fit(X)
-        assert np.all(scaler._mu == np.array([0,0,0]))
+        self.scaler.fit(self.X1)
+        assert np.all(self.scaler._mu == np.array([0,0,0]))
 
     def test_fit_transform(self):
-        X = np.array([[0,0,0],[2,2,2]])
-        scaler = MyStandardScaler()
-        ret = scaler.fit_transform(X)
+        ret = self.scaler.fit_transform(self.X2)
         assert np.all(ret == np.array([[-1,-1,-1],[1,1,1]]))
